9156ec2c902907043e5862a20bb4cfa00c14da7e
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-04-02 13:31:28 +0530

    String Methods

--- string_method.py
+++ string_method.py
@@ -1,4 +1,7 @@
-s = input("Enter a string: ")
+# s = input("Enter a 1st string: ")
+# s2 = input("Enter a 2nd string: ")
+# s3 = input("Enter a 3rd string: ")
+
 # print(s.lower())
 # print(s.upper())
 # print(s.title())
@@ -6,7 +9,45 @@ s = input("Enter a string: ")
 # print(s.startswith('p'))
 # print(s.endswith('n'))
 # print(s.split())
-# print(s.join('la'))
+# print(s.split('p'))
+# print(s.split(' ',1))
+# print(s.split(' ',0))
+# print(input("Enter any string: ").split(input("Enter a which part you want to split: "),int(input("Enter number of time your want to split: "))))
+
+# x=input("Enter any seperator: ")
+# iterable = collection
+# print(x.join([s1,s2,s3]))
 # print(s.index('p'))
 # print(s.count('p'))
 # print(s.capitalize())
+# print(s.strip('!'))
+# print(s.lstrip('!'))
+# print(s.rstrip('!'))
+
+
+# print(input("Enter a string").index(input("Enter any character: "),int(input("Enter Start point: ")),int(input("Enter endpoint: "))))
+
+# s = input("Enter a string: ")
+# ch = input("Entera ny character:")
+# print(s.count(ch))
+
+# print(input("Enter a string: ").count(input("Enter any character: ")))
+
+
+# s=input("enter a string: ")
+# print(s.isalnum())
+# print(s.isalpha())
+# print(s.isascii())
+# print(s.isdecimal())
+# print(s.isdigit())
+# print(s.isidentifier())
+# print(s.islower())
+# print(s.isnumeric())
+# print(s.isprintable())
+# print(s.isspace())
+# print(s.istitle())
+# print(s.isupper())
+
+# x=50
+# print(bool(x))
+
