426699fea5489bb6f406793a67949d5e485746a5
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-04-15 12:32:51 +0530

    Control Statement

---
+++ control_statement.py
@@ -0,0 +1,25 @@
+# Control Statement :- it is divided into 3 parts
+
+# 1. conditional statement :- if, if-else, if-elif, if-elif-else
+# 2. itrating/ looping statement :- while, while-else, for, for-else
+# 3. transfer statement :- continue, break, pass
+
+# elif is a dependent on if
+
+# ----------------------------------------------------------------------------------------------------
+
+
+# conditional statement :-
+
+# i :- if-statement :- syntax :- if condition :
+#                                    |
+#                                    |if-body executed when given condition is true
+#                                    |
+# Note :- block may be executed
+
+# Example
+# if 10:
+#     print('Hello')
+
+
+# ii :- if-else statement :- 
