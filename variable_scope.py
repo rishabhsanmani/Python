fca69296430c72889b0e438b4fefc2fed47f1c3d
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-05-12 12:59:24 +0530

    Variable Scope

--- variable_scope.py
+++ variable_scope.py
@@ -15,10 +15,40 @@
 # add()
 # print(x)
 
-x=10
-def add():
-    x=50
-    print(x)
-    print(globals()['x'])
-add()
+# x=10
+# def add():
+#     x=50
+#     print(x)
+#     print(globals()['x'])
+# add()
 # print(x)
+
+# do not create an scope
+
+# if
+# elif
+# else
+# try
+# except
+# while
+# for
+
+
+# n = eval(input("Enter any number : "))
+# if n>0 :
+#     x = 20
+# elif n<0 :
+#     y = 30
+# else:
+#     z = 10
+# print(x)
+# print(y)
+# print(z)
+
+
+
+# create new scope
+
+# function
+# class
+
