f3dc521ef4184451617f675af362587ba28279a0
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-05-13 11:57:14 +0530

    Higher Order Function

---
+++ higher_order_function.py
@@ -0,0 +1,134 @@
+# Functions of Hingher order function
+# map()
+# filter()
+# reduce()
+# decorator()
+
+# lambda
+# genarator
+# file-handling
+# module __package__ library
+# OOPs
+# exception handling
+
+
+# 1. Map()
+
+# syntax :- 
+
+# iterable1 -------------
+# iterable2 -------------
+
+# def fun_name(n1,n2,...):
+#     fun_body
+
+# map(fun_name(iterable1,iterable2))
+# map(fun,iterable)
+
+# l1 = [1,2,3,4]
+# l2 = [5,6,7,8]
+# l3 = [1,2,3,4]
+
+# def sum(n1,n2,n3):
+#     return n1+n2+n3
+
+# res = map(sum,l1,l2,l3)
+# print(res)
+# print(list(res))
+                            #   or
+# res = list(map(sum,l1,l2,l3))
+# print(res)
+
+
+
+# -----------------------------------------------------
+
+
+
+# 2. Filter function
+
+# syntax :- 
+
+# iterable
+# def fun_name(n):
+#     conditional statement
+
+# res = list(filter(fun_name,iterable))
+# filter(fun_name,iterable)            
+
+
+
+# l = [1,2,3,4,5,6,7,8]
+
+# def even(n):
+#     if n%2==0:
+#         return n
+    
+# def odd(n):
+#     if n%2!=0:
+#         return n
+
+# res = tuple(filter(even,l))
+# print(res)
+# res1 = tuple(filter(odd,l))
+# print(res1)
+
+
+# l = [1,2,3,4,5,6,7,8]
+
+# def show(n):
+#     if n%2==0:
+#         return 'Even'
+#     else :
+#         return 'Odd'
+
+# res = list(map(show,l))
+# print(res)
+
+
+
+# ---------------------------------------------
+
+
+
+# 3. Reduce function
+
+# syntax :-
+
+# import functools
+# iterable
+# def fun_name(n1,n2):
+#     fun_body
+# res = functools.reduce(fun_name,iterable)
+
+
+# import functools
+# l = [1,2,3,4,5]
+# def add(a,b):
+#     return a+b
+# red = functools.reduce(add,l,0)
+# print(red)
+# res = functools.reduce(add,l)
+# print(res)
+
+
+# import functools
+# l = [10,5,20,30,15,12]
+# def max(a,b):
+#     if a>b:
+#         return a
+#     else :
+#         return b
+# res = functools.reduce(max,l)
+# print(res)
+
+import functools
+l = [10,5,20,30,15,12]
+def min(a,b):
+    if a<b:
+        return a
+    else :
+        return b
+res = functools.reduce(min,l)
+print(res)
+
