4288a36e8b73e14d38023d80ef770c2a97265db8
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-05-18 20:01:50 +0530

    Pattern

---
+++ Pattern.py
@@ -0,0 +1,90 @@
+# Pattern
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print('*'*n)
+#     i+=1
+
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print('*'*i)
+#     i+=1
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print('*'*i+' '*(n-i))
+#     i+=1
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print(' '*(n-i)+'*'*i)
+#     i+=1
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print(' '*(n-i)+'* '*i)
+#     i+=1
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print(' '*(n-i)+' *'*i)
+#     i+=1
+
+
+# n = eval(input('Enter number: '))
+# i=0
+# while i<n:
+#     print('*'*(n-i)+' '*i)
+#     i+=1
+
+# n = eval(input('Enter number: '))
+# i=0
+# while i<n:
+#     print(' '*i+'*'*(n-i))
+#     i+=1
+
+# n = eval(input('Enter number: '))
+# i=0
+# while i<n:
+#     print(' '*i+'* '*(n-i))
+#     i+=1
+
+
+n = eval(input('Enter number : '))
+i=1 
+while i<=n:
+    print('*'*i+' '*(n-i))
+    i+=1
+i-=2
+while i>0:
+    print('*'*i+' '*(n-i))
+    i-=1
+
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print(' '*(n-i)+'*'*i)
+#     i+=1
+# i-=2
+# while i>0:
+#     print(' '*(n-i)+'*'*i)
+#     i-=1
+
+
+# n = eval(input('Enter number : '))
+# i=1 
+# while i<=n:
+#     print(' '*(n-i)+'* '*i)
+#     i+=1
+# i-=2
+# while i>0:
+#     print(' '*(n-i)+'* '*i)
+#     i-=1
